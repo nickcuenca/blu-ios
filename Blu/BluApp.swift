00c6211b5985ac5febe610c67e586c7bc102c10d
“nickcuenca” <72580887+nickcuenca@users.noreply.github.com>
2025-03-27 21:58:53 -0700

    Initial Commit

---
+++ Blu/BluApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluApp.swift
+//  Blu
+//
+//  Created by Nicolas Cuenca on 3/27/25.
+//
+
+import SwiftUI
+
+@main
+struct BluApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BluTests/BluTests.swift
@@ -0,0 +1,16 @@
+//
+//  BluTests.swift
+//  BluTests
+//
+//  Created by Nicolas Cuenca on 3/27/25.
+//
+
+import Testing
+
+struct BluTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
