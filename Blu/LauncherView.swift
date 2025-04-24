6e09fd26e271627428218986374b199a9c32c749
“nickcuenca” <nicolascuenca123@gmail.com>
2025-04-23 17:05:02 -0700

    Added app Launch Screen + Fixed App Logo

--- Blu/LauncherView.swift
+++ Blu/LauncherView.swift
@@ -9,12 +9,32 @@ import SwiftUI
 
 struct LauncherView: View {
     @AppStorage("username") var username: String = ""
+    @State private var isLoading = true
 
     var body: some View {
-        if username.isEmpty {
-            RegistrationViewV2()
-        } else {
-            TabBarView()
+        Group {
+            if isLoading {
+                VStack {
+                    Image("Blu_Logo") // must match asset name
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 160, height: 160)
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color.white)
+                .ignoresSafeArea()
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                        isLoading = false
+                    }
+                }
+            } else {
+                if username.isEmpty {
+                    RegistrationViewV2()
+                } else {
+                    TabBarView()
+                }
+            }
         }
     }
 }
